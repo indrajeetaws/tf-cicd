3ca5a38b7cd3433c2e7e8cce0c85adfe3c2d290b
indrajeetaws <30336802+indrajeetaws@users.noreply.github.com>
2021-01-09 10:38:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 
 terraform {
     backend "azurerm" {
-        resource_group_name     =   "Jenkins"
+        resource_group_name     =   "cloud-shell-storage-centralindia"
         storage_account_name    =   "csg100320010980dd00"
         container_name          =   "tfremote"
         key                     =   "terraform.tfstate"
