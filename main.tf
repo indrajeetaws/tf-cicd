beadda85e67e4b44ed87da4dc62ad884ae11b40b
indrajeetaws <30336802+indrajeetaws@users.noreply.github.com>
2021-01-09 10:50:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,21 +21,18 @@ provider "azurerm" {
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
     tenant_id       =   var.tenant_id
-    
-    features {
-        virtual_machine {
-            delete_os_disk_on_deletion = true
-        }
-    }
 }
-
-provider "azuread" {
-    version         =   ">= 0.11"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    tenant_id       =   var.tenant_id
-    alias           =   "ad"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
+#provider "azuread" {
+#    version         =   ">= 0.11"
+#    client_id       =   var.client_id
+#    client_secret   =   var.client_secret
+#    tenant_id       =   var.tenant_id
+#    alias           =   "ad"
+#}
 
 
 // module "windows-server" {
