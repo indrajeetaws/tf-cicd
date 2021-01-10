e6a4fcf7d45b9132c79ab7032a70145792df214c
indrajeetaws <30336802+indrajeetaws@users.noreply.github.com>
2021-01-10 19:19:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ terraform {
         storage_account_name    =   "csg100320010980dd00"
         container_name          =   "tfremote"
         key                     =   "terraform.tfstate"
+        client_id       =   var.client_id
+        client_secret   =   var.client_secret
+        subscription_id =   var.subscription_id
+        tenant_id       =   var.tenant_id
     }
 }
 
